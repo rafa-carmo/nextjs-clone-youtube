e3a40cc14378d562e7d2fd57b6c5cdbb79af63fb
Rafael <rafaelcarmo_15@hotmail.com>
2021-04-15 20:19:30 -0300

    material-ui configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
 import React from "react";
+import {Button} from "@material-ui/core"
 
 export default function Home() {
   return (
-    <div> Nada </div>
+    <div> 
+<Button variant="contained" color="primary">
+  Primary
+</Button>
+      Nada </div>
   );
 }
